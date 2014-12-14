4799de457ef99e6fe5aa8cf0eafe9e7a7e656458
fakhitah3 <fakhitah.ridzuan@gmail.com>
2014-12-14 12:53:18 +0800

    Add Login Function

---
+++ NetBeansProjects/Training Management System/src/java/coreservlets/loginBean.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coreservlets;
+
+/**
+ *
+ * @author Asus
+ */
+public class loginBean {
+ String uname;
+ String pwd;
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+ 
+}
